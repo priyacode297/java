e86a0d869203a8f80dceee7698cb6db441deb050
priyacode297 <singulurijeevana@gmail.com>
2026-06-03 22:07:35 +0530

    Add java program to count even numbers in an array

---
+++ CountEvenNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class CountEvenNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] nums = new int[5];
+
+        System.out.println("Enter the five numbers:");
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("The Even numbers are: " + count);
+    }
+}
